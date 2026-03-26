aab2d78c636a055bf7a591a8a3acda286e1eb7ab
ANTONIO COSTA SATIRO DE SOUZA <10723636@mackenzista.com.br>
2026-03-26 15:10:28 +0000

    Permuta terminado

--- Permuta.c
+++ Permuta.c
@@ -1,25 +1,57 @@
 #include <stdio.h>
 #include <stdbool.h>
-#define TAM 8
+#define TAM 4
 
 
-
-
-
-bool backtrack(int lados[4], int fosforos[TAM], int indicesAcessados[TAM], int , int afosforo, int ladoAtual, int alvo){
+void backtrack(int valores[TAM], int indicesAcessados[TAM], int indiceAtual, int s[TAM], int *max){
     
-    for (int i = 0; i < qfosforos; i++){
-        
-        bool checa = true;
-        
-        for (int j = 0; j < afosforo ; j++){
-            if (i == indicesAcessados[j]){
-                checa = false;
+    int soma = 0;
+
+    if (indiceAtual == TAM){
+
+        for (int i = 0; i < TAM - 1; i++){
+
+            int permuta = s[i] - s[i + 1];
+            if (permuta < 0){
+                permuta = permuta * - 1;
+                
             }
+            soma += permuta;
         }
-        
-        
+
+        if (soma > *max){
+            *max = soma;
         }
-    
-    return false;
+
+        
+
+    }
+
+    for (int i = 0; i < TAM; i++){
+
+        if (indicesAcessados[i] == 0){
+
+            indicesAcessados[i] = 1;
+            s[indiceAtual] = valores[i];
+            backtrack(valores, indicesAcessados, indiceAtual + 1, s, max);
+            indicesAcessados[i] = 0;
+
+        }
+
+    }
+}
+
+int main(){
+
+    int valores[TAM] = {4, 5, 1, 2};
+    int indicesAcessados[TAM];
+    int max= 0;
+    int s[TAM];
+
+    for (int i = 0; i < TAM; i++){
+        indicesAcessados[i] = 0;
+    }
+
+    backtrack(valores, indicesAcessados, 0, s, &max);
+    printf("%i\n", max);
 }
